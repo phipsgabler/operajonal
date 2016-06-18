722f704f7c7464079a66540b9f1a7625893535f5
Philipp Gabler <phips.gabler@gmail.com>
2016-06-18 20:42:58 +0200

    Resolved error with this not being bound in lambdas

--- examples/stacks.js
+++ examples/stacks.js
@@ -1,21 +1,39 @@
 'use strict';
 
-const op = require('../index.js');
+const {Program, makeInstructions} = require('../index.js');
+const daggy = require('daggy');
 
-const Push = n => op.Instruction('Push', {value: n});
-const Pop = op.Instruction('Pop');
-const Add = op.Instruction('Add');
+//const Push = n => op.Program.emit({type: 'Push', value: n});
+//const Pop = op.Program.emit({type: 'Pop'});
+//const Add = op.Program.emit({type: 'Add'});
+
+const StackInstr = makeInstructions({
+  Push: ['value'],
+  Pop: [],
+  Add: []
+});
+
+const {Push, Pop, Add} = StackInstr;
+
+//const StackInstr = daggy.taggedSum({
+//  Push: ['value'],
+//  Pop: [],
+//  Add: []
+//});
+//
+//const Push = n => op.Program.emit(StackInstr.Push(n));
+//const Pop = op.Program.emit(StackInstr.Pop);
+//const Add = op.Program.emit(StackInstr.Add);
 
 
-
-const testProgram1 = op.program(function*() {
-    yield Push(10);
-    yield Push(20);
-    yield Add;
-    yield Push(33);
-    let thirtyTwo = yield Pop;
-    let sum = yield Pop;
-    return `last element: ${thirtyTwo}, sum of previous: ${sum}`;
+const testProgram1 = Program.do(function*() {
+  yield Push(10);
+  yield Push(20);
+  yield Add;
+  yield Push(33);
+  let thirtyTwo = yield Pop;
+  let sum = yield Pop;
+  return `last element: ${thirtyTwo}, sum of previous: ${sum}`;
 });
 
 // the same thing in "verbose syntax":
@@ -27,69 +45,21 @@ const testProgram1 = op.program(function*() {
 //             sum => Program.inject([sum, thirtyTwo])));
 
 
-// const stackInterpreter = stack =>
-//       new Interpreter().on('Push', (p, v, recur) => {
-          
-//       });
 
-function interpretStackProgram(program, initialStack) {
-    const view = program.view;
+ const interpreter = (program, initialStack) => Program.interpret(program)({
+     Pop: recur => {
+         const [first, ...rest] = initialStack;
+         return interpreter(recur(first), rest);
+     },
+     Push: (recur, x) => interpreter(recur({}), [x, ...initialStack]),
+     Add: recur => {
+         const [first, second, ...rest] = initialStack;
+         const sum = first + second;
+         return interpreter(recur(sum), [sum, ...rest]);
+     }
+ });
 
-    switch (view.type) {
-    case 'Return':
-        return view.value;
-    case 'Continue':
-        switch (view.instr.type) {
-        case 'Push': {
-            return interpretStackProgram(view.cont({}), [view.instr.value, ...initialStack]);
-        }
-        case 'Pop': {
-            const [first, ...rest] = initialStack;
-            return interpretStackProgram(view.cont(first), rest);
-        }
-        case 'Add': {
-            const [first, second, ...rest] = initialStack;
-            const sum = first + second;
-            return interpretStackProgram(view.cont(sum), [sum, ...rest]);
-        }
-        default:
-            throw new Error(`Invalid view: ${JSON.stringify(view)}`);
-        }
-    default:
-        throw 'Error';
-    }
-}
-
-
-// ideas for better syntax: 
-// const stackInterpreter = initialStack => Interpreter([
-//     on('Return')(x => x),
-//     on('Pop')(recur => {
-//         const [first, ...rest] = initialStack;
-//         return stackInterpreter(recur(first), rest);
-//     }),
-//     on('Push')((recur, x) => stackInterpreter(recur({}), [x, ...initialStack])),
-//     on('Add')(recur => {
-//         const [first, second, ...rest] = initialStack;
-//         const sum = first + second;
-//         return stackInterpreter(recur(sum), [sum, ...rest]);
-//     })
-// ]);
-
-// const interpreter = initialStack => Interpreter({
-//     Return: x => x,
-//     Pop: recur => {
-//         const [first, ...rest] = initialStack;
-//         return recur(first, rest);
-//     },
-//     Push: recur, x => recur({}, [x, ...initialStack]);
-//     Add: recur => {
-//         const [first, second, ...rest] = initialStack;
-//         const sum = first + second;
-//         return recur(sum, [sum, ...rest]);
-//     }
-// });
-
-
-console.log(interpretStackProgram(testProgram1, []));
+//
+//
+console.log(interpreter(testProgram1, []));
 
--- index.js
+++ index.js
@@ -1,82 +1,9 @@
 'use strict';
 
 const daggy = require('daggy');
+const _ = require('underscore');
 
 // see: http://hackage.haskell.org/package/operational-0.2.3.2/docs/Control-Monad-Operational.html
-
-// Basic representation of free programs
-const Program = daggy.taggedSum({
-  Lift: ['value'],
-  Bind: ['action', 'continuation'],
-  Instr: ['instruction']
-});
-
-const {Lift, Bind, Instr} = Program;
-
-Program.of = x => Lift(x);
-
-Program.prototype.chain = f => Bind(this, f);
-
-Program.prototype.map = f => Bind(this, x => Program.of(f(x)));
-
-Program.prototype.andThen = k => Bind(this, _ => k);
-
-Program.prototype.view = () => this.cata({
-  Lift: value => Return(x),
-  Bind: (action, continuation) => action.cata({
-    Lift: value => continuation(value).view,
-    Bind: (action2, continuation2) =>
-      Bind(action2, x => Bind(continuation2(x), continuation)).view,
-    Instr: instruction => Continue(instruction, continuation)
-  }),
-  Instr: instruction => Continue(instruction, Program.of)
-});
-
-// for (let constr in Program) {
-//   Object.assign(constr, {
-//     get view() {
-//       return constr.cata({
-//         Lift: value => Return(x),
-//         Bind: (action, continuation) => action.cata({
-//           Lift: value => continuation(value).view,
-//           Bind: (action2, continuation2) =>
-//             Bind(action2, x => Bind(continuation2(x), continuation)).view,
-//           Instr: instruction => Continue(instruction, continuation)
-//         }),
-//         Instr: instruction => Continue(instruction, Program.of)
-//       });
-//     }
-//   });
-// }
-
-// Object.defineProperty(Program.prototype, 'view', {
-//   get: () => this.cata({
-//     Lift: value => Return(x),
-//     Bind: (action, continuation) => action.cata({
-//       Lift: value => continuation(value).view,
-//       Bind: (action2, continuation2) =>
-//         Bind(action2, x => Bind(continuation2(x), continuation)).view,
-//       Instr: instruction => Continue(instruction, continuation)
-//     }),
-//     Instr: instruction => Continue(instruction, Program.of)
-//   })
-// });
-
-// Object.assign(Program.prototype, {
-//   get view() {
-//     return this.cata({
-//       Lift: value => Return(x),
-//       Bind: (action, continuation) => action.cata({
-//         Lift: value => continuation(value).view,
-//         Bind: (action2, continuation2) =>
-//           Bind(action2, x => Bind(continuation2(x), continuation)).view,
-//         Instr: instruction => Continue(instruction, continuation)
-//       }),
-//       Instr: instruction => Continue(instruction, Program.of)
-//     });
-//   }
-// }
-
 
 // Views of programs, used to interpret them
 const ProgramView = daggy.taggedSum({
@@ -87,29 +14,96 @@ const ProgramView = daggy.taggedSum({
 const {Return, Continue} = ProgramView;
 
 
-// see: https://github.com/Risto-Stevcev/do-notation
-function programDo(generatorFunction) {
-    const generator = generatorFunction();
-    
-    return function next(error, v) {
-        const res = generator.next(v);
-        
-        if (res.done) {
-            return Program.of(res.value);
-        } else {
-            return res.value.chain(v => next(null, v) || Program.of(v));
-        }
-    }()
+// Internal representation of free programs over instructions
+const Program = daggy.taggedSum({
+  Lift: ['value'],
+  Bind: ['action', 'continuation'],
+  Instr: ['instruction']
+});
+
+const {Lift, Bind, Instr} = Program;
+
+Program.of = x => Lift(x);
+
+Program.emit = x => Instr(x);
+
+Program.prototype.chain = function(f) {
+  console.log(this);
+  return Bind(this, f);
+};
+
+Program.prototype.map = function(f){
+  return Bind(this, x => Program.of(f(x)));
+};
+
+Program.prototype.andThen = function(k) {
+  Bind(this, _ => k);
+};
+
+
+
+function viewProgram(program) {
+  return program.cata({
+    Lift: value => Return(x),
+    Bind: (action, continuation) => action.cata({
+      Lift: value => viewProgram(continuation(value)),
+      Bind: (action2, continuation2) =>
+          viewProgram(Bind(action2, x => Bind(continuation2(x), continuation))),
+      Instr: instruction => Continue(instruction, continuation)
+    }),
+    Instr: instruction => Continue(instruction, Program.of)
+  })
 }
 
-// exports.instructionSet = function(name, value) {
-//     return new Instr(Object.assign({type: name}, value));
-// }
+Program.interpret = prog => interpretation => {
+  const returner = interpretation.Return || (x => x);
+
+  viewProgram(prog).cata({
+    Return: x => returner(x),
+    Continue: (instruction, continuation) =>
+        instruction.cata(_.mapObject(interpretation, e => _.partial(e, continuation, _)))
+  });
+};
+
+// see: https://github.com/Risto-Stevcev/do-notation
+Program.do = function (generatorFunction) {
+  const generator = generatorFunction();
+
+  return function next(error, v) {
+    const res = generator.next(v);
+
+    if (res.done) {
+      return Program.of(res.value);
+    } else {
+      return res.value.chain(v => next(null, v) || Program.of(v));
+    }
+  }()
+};
+
+
+
+function makeInstructions(constructors) {
+  //return new Instr(Object.assign({type: name}, value));
+
+  function instructions() {
+    throw new TypeError('Instruction was called instead of one of its properties.');
+  }
+
+  const representation = daggy.taggedSum(constructors);
+
+  for (let key in constructors) {
+    if (!constructors[key].length)
+        instructions[key] = Instr(representation[key]);
+    else
+        instructions[key] = (...args) => Instr(representation[key](...args));
+  }
+
+  return instructions;
+}
 
 module.exports = {
   Program,
-  ProgramView,
-  programDo
+  makeInstructions
 };
 
 
